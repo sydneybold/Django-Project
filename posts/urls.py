e3ffcfac160547329d5081513db06239382f7941
Sydney Bold <sydneybold@Sydneys-MBP-2.bishops.com>
2021-05-24 11:22:32 -0700

    Fix a bug in the urls for the posts

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "posts"
 urlpatterns = [
     path('', views.PostFeedView.as_view(), name='feed'),
     path('posts/new/', views.CreatePostView.as_view(), name='create_post'),
