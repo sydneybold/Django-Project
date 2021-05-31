777be0054607885b7b01a35815018e48b61c235c
Sydney Bold <sydneybold@Sydneys-MacBook-Pro-2.local>
2021-05-30 21:02:22 -0700

    Change the title atribute to the atribute caption

--- posts/models.py
+++ posts/models.py
@@ -4,11 +4,11 @@ from users.models import Profile
 class Post(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
-    title = models.CharField(max_length=255)
+    caption = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='posts/photos')
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.caption
