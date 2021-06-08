a2618a966f596d0bee5ccdb6ec1920f612ea6278
Sydney Bold <sydneybold@Sydneys-MacBook-Pro-2.local>
2021-06-07 22:45:09 -0700

    Delete the password validation

--- users/forms.py
+++ users/forms.py
@@ -11,15 +11,13 @@ from django.contrib.auth.models import User
 class SignupForm(forms.ModelForm):
     username = forms.CharField(label='Username', min_length=5, max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(label='Password', min_length=8, max_length=15, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password_confirmation = forms.CharField(label='Password Confirmation', min_length=8, max_length=15, widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
     first_name = forms.CharField(label='First Name', min_length=2, max_length=20, widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
     last_name = forms.CharField(label='Last Name', min_length=2, max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    email = forms.CharField(label='Email', min_length=5, max_length=20, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    email = forms.CharField(label='Email', min_length=5, max_length=40, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
 
     class Meta:
     	model = User
-    	fields = ['username', 'password', 'password_confirmation', 'first_name', 'last_name', "email"]
-
+    	fields = ['username', 'password', 'first_name', 'last_name', "email"]
 
 class UpdateForm(forms.ModelForm):
     class Meta:
