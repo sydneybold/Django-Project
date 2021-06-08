2104b669f3f994ab97827e496289552bd1eb3755
Sydney Bold <sydneybold@Sydneys-MacBook-Pro-2.local>
2021-06-07 20:23:51 -0700

    Update the CreatePostView to process the data from the form

--- posts/views.py
+++ posts/views.py
@@ -1,29 +1,37 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+
+
 
 # Models
 from posts.models import Post
 
+# Forms
+from posts.forms import PostForm
 
-# change to generic.CreateView
-class CreatePostView(generic.ListView):
+
+class CreatePostView(generic.CreateView):
     template_name = 'posts/new.html'
-    # form_class = PostForm
-    context_object_name = 'form'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
 
-    def get_queryset(self):
-        return Post.objects
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class PostFeedView(generic.ListView):
-    template_name = 'posts/feed.html'
     model = Post
+    template_name = 'posts/feed.html'
     context_object_name = 'posts'
-
+    ordering = ('-created',)
+    paginate_by = 4
 
 class PostDetailView(generic.DetailView):
+    model = Post
     template_name = 'posts/detail.html'
-    slug_field = 'title'
-    slug_url_kwarg = 'post_title'
-    queryset = Post.objects.all()
-    context_object_name = 'post'
