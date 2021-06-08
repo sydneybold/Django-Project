bcc044a19c135753107ffb0a15061de333e5e2fa
Sydney Bold <sydneybold@Sydneys-MacBook-Pro-2.local>
2021-06-07 20:16:46 -0700

    Change the urls to include the pk

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from . import views
 
+from users.views import *
+
+app_name = "users"
 urlpatterns = [
-    path('', views.PostFeedView.as_view(), name='feed'),
-    path('posts/new/', views.CreatePostView.as_view(), name='create_post'),
-    path('posts/<slug:post_title>/', views.PostDetailView.as_view(), name='detail'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('<pk>/update', UpdateProfileView.as_view(), name='update_profile'),
+    path('<pk>', login_required(UserDetailView.as_view()), name='detail'),
 ]
